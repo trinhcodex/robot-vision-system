0658ab636c842862a20d950dca38299498f6c577
thanhhung0112 <thanhhungthh987@gmail.com>
2023-05-14 16:50:30 +0700

    modify

--- 7. OUTPUT/SOURCE_CODE/cvu_program/human_interface/winform-application-demo/Template-Matching/request.sh
+++ 7. OUTPUT/SOURCE_CODE/cvu_program/human_interface/winform-application-demo/Template-Matching/request.sh
@@ -1,7 +1,7 @@
 curl -X POST -H "Content-Type: multipart/form-data" \
-			 -F "api_folder=/home/kratosth/code/Template-Matching" \
+			 -F "api_folder=/home/kratosth/code/robot-vision-system/7. OUTPUT/SOURCE_CODE/cvu_program/human_interface/winform-application-demo/Template-Matching" \
 			 -F "output_folder=Output" \
-			 -F "img_path=Dataset/Src1.bmp" \
+			 -F "img_path=Dataset/Src2.bmp" \
 			 -F "template_path=Dataset/20220611.bmp" \
 			 -F "threshold=0.75" \
 			 -F "overlap=0.4" \
@@ -9,5 +9,5 @@ curl -X POST -H "Content-Type: multipart/form-data" \
 			 -F "min_modify=-1" \
 			 -F "max_modify=1" \
 			 -F "enhance=Custom_enhance/Src1-2.json" \
-			 -F "representation=Custom_representation/Src1-2.json" \
+			 -F "representation=Custom_representation/Src3-5-8-9-10.json" \
 			 http://127.0.0.1:5000/my_cvu_api
